a801556ade2cfd81a41efb24475fc2f3dac389c2
Ananda Guedes <anandaguedesdoo@gmail.com>
2025-09-22 16:12:37 -0300

    primeira aula de RAD

--- Python/YoutubeDownload/youtube_download.py
+++ Python/YoutubeDownload/youtube_download.py
@@ -1,12 +1,12 @@
-from pytubefix import Youtube
+from pytubefix import YouTube
 from pytubefix.cli import on_progress
 from pathlib import Path
 
-url = ""
+url = "https://www.youtube.com/watch?v=Cd8-OOlliaY&list=RDCd8-OOlliaY&start_radio=1"
 destino = Path("videos_youtube")
 destino.mkdir(exist_ok=True)
 
-yt = Youtube(url, on_progress_callback=on_progress)
+yt = YouTube(url, on_progress_callback=on_progress)
 print(f'Título: {yt.title}\nDuração: {yt.length}s')
 
 yt.streams.get_highest_resolution().download(output_path=destino)
